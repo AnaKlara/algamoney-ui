1a499f8f2755629d471408d66ea6180a547be4b5
Ana <ana.clara.correa.da.silva@ibm.com>
2020-05-19 09:58:39 -0300

    Atende a verificações do ng build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://algam-api.herokuapp.com/', 
-  whitelistedDomains: [/localhost:8080/ ],//lista de dominios que o token será enviado (não pode ser enviado para qualquer lugar)
-  blacklistedRoutes: [ /\/oauth\/token/]
+  whitelistedDomains: [new RegExp('/localhost:8080/') ],//lista de dominios que o token será enviado (não pode ser enviado para qualquer lugar)
+  blacklistedRoutes: [ new RegExp('/\/oauth\/token/')]
 };
